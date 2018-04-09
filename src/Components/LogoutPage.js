5c96d51687face3e155d8b01e055c4b93af789ec
PPbra <16021629@vnu.edu.vn>
2018-04-09 14:25:38 +0700

    edit import

---
+++ src/Components/LogoutPage.js
@@ -0,0 +1,20 @@
+import React , {Component} from "react";
+import Redirect from "react-router-dom/es/Redirect";
+import {deleteTokenFromStorage} from "../Services/LocalStorageServices";
+
+class LogoutPage extends React.Component{
+
+    render(){
+
+/*        this.props.appAuthChange(false);*/
+        deleteTokenFromStorage();
+
+        return(
+            <Redirect to={'./login'}/>
+        )
+    }
+
+
+}
+
+export  default LogoutPage;
